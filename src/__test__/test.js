547f36aa1fc28a3879bd7c0fe7f32d096608ae8c
Evgeniya Malashenko <malashenko_genya@mail.ru>
2023-10-07 20:35:03 +0300

    first commit Unit_Testing_Matchers

---
+++ src/__test__/test.js
@@ -0,0 +1,33 @@
+import SortHealthLevel from '../sortinglevel';
+
+test('sort test', () => {
+  const arrTest = [
+    { name: 'мечник', health: 10 },
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+  ];
+  const arrRight = [
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+    { name: 'мечник', health: 10 },
+  ];
+
+  SortHealthLevel(arrTest);
+  expect(arrTest).toEqual(arrRight);
+});
+
+test('sort test not tobe', () => {
+    const arrTest = [
+        { name: 'мечник', health: 10 },
+        { name: 'маг', health: 100 },
+        { name: 'лучник', health: 80 },
+    ];
+    const arrRight = [
+        { name: 'маг', health: 100 },
+        { name: 'лучник', health: 80 },
+        { name: 'мечник', health: 10 },
+    ];
+
+    SortHealthLevel(arrTest);
+    expect(arrTest).not.toBe(arrRight);
+});
---
+++ src/sortinglevel.js
@@ -0,0 +1,11 @@
+export default function SortHealthLevel(arrObject) {
+    arrObject.sort((a, b) => b.health - a.health);
+  }
+  
+  SortHealthLevel(
+    [
+      { name: 'маг', health: 100 },
+      { name: 'лучник', health: 80 },
+      { name: 'мечник', health: 10 },
+    ],
+  );
